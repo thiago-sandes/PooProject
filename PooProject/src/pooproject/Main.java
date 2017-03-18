156ea23e556507769bc27a3f003870c0ff3280a2
thiago-sandes <thiago-sandes@hotmail.com>
2017-03-17 21:49:47 -0300

    VERSÃO ULTIMATE DO PROJETO. SUJEITA A REVISÃO DE CÓDIGO E CRIAÇÃO DE DOCUMENTAÇÃO.

--- PooProject/src/pooproject/Main.java
+++ PooProject/src/pooproject/Main.java
@@ -8,103 +8,105 @@ package pooproject;
 import java.util.Scanner;
 
 /**
- * #TODO
- * Line 174. Fazer outra função para tratar a leitura dos horários de consulta. 
- * 
+ * #TODO Line 174. Fazer outra função para tratar a leitura dos horários de
+ * consulta.
+ *
  * @author thiago_sandes
- */ 
-public class Main  {
-    public static void main(String args[]){
+ */
+public class Main {
+
+    public static void main(String args[]) {
         int opcaoMenu, opcaoMenuPrincipal;
         String nome, aux;
         boolean flagVoltar = false;
         Agenda agenda = new Agenda();
         Scanner ler = new Scanner(System.in);
-         
+
         agenda.menuPrincipal();
-        
+
         System.out.println("\nInsira a opcao desejada:");
         opcaoMenuPrincipal = agenda.lerOpcao();
 
-        while(opcaoMenuPrincipal > 4 || opcaoMenuPrincipal < 1){
+        while (opcaoMenuPrincipal > 4 || opcaoMenuPrincipal < 1) {
 
-                System.out.println("Opcao errada. Tente novamente");
-                System.out.println("\nInsira a opcao desejada:");
-                opcaoMenuPrincipal = agenda.lerOpcao();
+            System.out.println("Opcao errada. Tente novamente");
+            System.out.println("\nInsira a opcao desejada:");
+            opcaoMenuPrincipal = agenda.lerOpcao();
 
         }
 
         opcaoMenu = opcaoMenuPrincipal;
 
-        while(opcaoMenu > 0 && opcaoMenu < 6){
+        while (opcaoMenu > 0 && opcaoMenu < 6) {
 
-            if(flagVoltar == true){
+            if (flagVoltar == true) {
                 System.out.println("\nInsira a opcao desejada:");
                 opcaoMenuPrincipal = agenda.lerOpcao();
             }
 
-            switch(opcaoMenuPrincipal){
+            switch (opcaoMenuPrincipal) {
                 case 1:
                     agenda.menuPaciente();
                     System.out.println("\nInsira a opcao desejada:");
                     opcaoMenu = agenda.lerOpcao();
 
+                    switch (opcaoMenu) {
+                        case 1:
+                            Paciente paciente = new Paciente();
+                            String cpf,
+                             telefone;
 
-                        switch(opcaoMenu){                
-                            case 1:
-                                Paciente paciente = new Paciente();
-                                String cpf, telefone;
+                            System.out.println("Para adicionar um paciente, insira seu nome: ");
+                            nome = ler.nextLine();
+                            paciente.setNome(nome);
+                            System.out.println("Agora, insira seu cpf: ");
+                            cpf = ler.nextLine();
+                            paciente.setCpf(cpf);
+                            System.out.println("Por fim, insira seu numero de telefone ou celular: ");
+                            telefone = ler.nextLine();
+                            paciente.setTelefone(telefone);
+                            agenda.addPaciente(nome, paciente);
+                            System.out.println("\nPaciente e seus respectivos dados adicionados com sucesso!\n");
 
-                                System.out.println("Para adicionar um paciente, insira seu nome: ");
-                                nome = ler.nextLine();
-                                paciente.setNome(nome);
-                                System.out.println("Agora, insira seu cpf: ");
-                                cpf = ler.nextLine();
-                                paciente.setCpf(cpf);
-                                System.out.println("Por fim, insira seu numero de telefone ou celular: ");
-                                telefone = ler.nextLine();
-                                paciente.setTelefone(telefone);
-                                agenda.addPaciente(nome, paciente);
-                                System.out.println("\nPaciente e seus respectivos dados adicionados com sucesso!\n");
+                            agenda.voltarMenuPrincipal();
+                            flagVoltar = true;
+                            break;
+                        case 2:
+                            System.out.println("Para remover um paciente, insira seu nome: ");
+                            nome = ler.nextLine();
+                            agenda.removerPacientePorNome(nome);
+                            agenda.voltarMenuPrincipal();
+                            flagVoltar = true;
+                            break;
+                        case 3:
 
-                                agenda.voltarMenuPrincipal();
-                                flagVoltar = true;
-                                break;
-                            case 2:
-                                System.out.println("Para remover um paciente, insira seu nome: ");
-                                nome = ler.nextLine();
-                                agenda.removerPacientePorNome(nome);
-                                agenda.voltarMenuPrincipal();
-                                flagVoltar = true;
-                                break;
-                            case 3:
-
-                                System.out.println("Insira o nome do paciente:");
-                                nome = ler.nextLine();
-                                agenda.listarPacientePorNome(nome);
-                                agenda.voltarMenuPrincipal();
-                                flagVoltar = true;
-                                break;
-                            case 4:
-                                agenda.listarTodosPacientes();
-                                agenda.voltarMenuPrincipal();
-                                flagVoltar = true;
-                                break;
-                            case 5:
-                                agenda.menuPrincipal();
-                                flagVoltar = true;
-                                break;
-                        }
+                            System.out.println("Insira o nome do paciente:");
+                            nome = ler.nextLine();
+                            agenda.listarPacientePorNome(nome);
+                            agenda.voltarMenuPrincipal();
+                            flagVoltar = true;
+                            break;
+                        case 4:
+                            agenda.listarTodosPacientes();
+                            agenda.voltarMenuPrincipal();
+                            flagVoltar = true;
+                            break;
+                        case 5:
+                            agenda.menuPrincipal();
+                            flagVoltar = true;
+                            break;
+                    }
                     break;
                 case 2:
                     agenda.menuMedico();
                     System.out.println("\nInsira a opcao desejada:");
                     opcaoMenu = agenda.lerOpcao();
 
-                    switch(opcaoMenu){                
+                    switch (opcaoMenu) {
                         case 1:
                             Medico medico = new Medico();
-                            String especialidade, nomeMed;
+                            String especialidade,
+                             nomeMed;
 
                             System.out.println("Para adicionar um medico, insira seu nome: ");
                             nomeMed = ler.nextLine();
@@ -153,46 +155,62 @@ public class Main  {
                     agenda.menuConsulta();
                     System.out.println("\nInsira a opcao desejada:");
                     opcaoMenu = agenda.lerOpcao();
-                    int horario; // Os horários de atendimentos são das 08h - 12h (1h cada consulta) e das 14h - 18h.
-                    String nomePaciente, nomeMedico;
+                    int horario = 0; // Os horários de atendimentos são das 08h - 12h (1h cada consulta) e das 14h - 18h.
+                    String nomePaciente,
+                     nomeMedico,
+                     auxHorario;
                     boolean auxFlag;
-                    
-                    switch(opcaoMenu){                
+
+                    switch (opcaoMenu) {
                         case 1: // Criar hashMap de Consultas...
                             Consulta consulta = new Consulta();
                             System.out.println("Insira o nome do paciente cadastrado:");
                             nomePaciente = ler.nextLine();
                             auxFlag = agenda.listarPacientePorNome(nomePaciente);
-                            if(auxFlag == true){    
+                            if (auxFlag == true) {
                                 consulta.setNomePaciente(nomePaciente);
                                 System.out.println("Deseja marcar a consulta para qual médico(insira seu nome)?:");
                                 nomeMedico = ler.nextLine();
                                 auxFlag = agenda.listarMedicoPorNome(nomeMedico);
-                                if(auxFlag == true){
+                                if (auxFlag == true) {
                                     consulta.setNomeMedico(nomeMedico);
                                     System.out.println("Os horários de atendimentos são das 08h - 12h (1h cada consulta) e das 14h - 18h. Em qual horario deseja marcar a consulta?:");
-                                    horario = agenda.lerOpcao();
-                                    while((horario > 11 && horario < 14) || (horario > 17 || horario < 8)){
-                                        System.out.println("Horário inválido. Tente novamente com um horario disponivel: ");
-                                        horario = agenda.lerOpcao();
+
+                                    try {
+                                        auxHorario = ler.nextLine();
+                                        horario = Integer.parseInt(auxHorario);
+
+                                        while ((horario > 11 && horario < 14) || (horario > 17 || horario < 8)) {
+                                            System.out.println("Horário inválido. Tente novamente com um horario disponivel: ");
+                                            auxHorario = ler.nextLine();
+                                            horario = Integer.parseInt(auxHorario);
+                                            auxFlag = true;
+                                        }
+                                    } catch (NumberFormatException e) {
+                                        System.out.println("Caractere invalido. Tente novamente acessando o menu Consulta.");
+                                        auxFlag = false;
                                     }
-                                    consulta.setHorario(horario);
-                                    
-                                    System.out.println("\nMarcando a consulta . . . \n");
-                                    agenda.addConsulta(nomePaciente, consulta);
-                                    agenda.delay(3);
-                                    
-                                    System.out.println("\nConsulta registrada com sucesso!\n");
-                                    
-                                }else{
+                                    if (auxFlag == true) {
+                                        consulta.setHorario(horario);
+
+                                        System.out.println("\nMarcando a consulta . . . \n");
+                                        agenda.addConsulta(nomePaciente, consulta);
+                                        agenda.delay(3);
+
+                                        System.out.println("\nConsulta registrada com sucesso!\n");
+                                    } else {
+                                        System.out.println("\nConsulta não registrada!\n");
+                                        agenda.delay(3);
+                                    }
+                                } else {
                                     System.out.println("Busque o nome correto do medico no menu Medico e volte aqui.");
-                                
+
                                 }
-                            }else{
+                            } else {
                                 System.out.println("Busque o nome correto do paciente no menu Paciente e volte aqui.");
-                                
+
                             }
-                            
+
                             agenda.voltarMenuPrincipal();
                             flagVoltar = true;
 
@@ -203,21 +221,21 @@ public class Main  {
                             agenda.removerConsultaPorPaciente(nome);
 
                             agenda.voltarMenuPrincipal();
-                            flagVoltar = true;    
+                            flagVoltar = true;
 
                             break;
                         case 3:
                             System.out.println("Insira o nome do paciente para listar suas consultas:");
                             nome = ler.nextLine();
                             agenda.listarConsultaPorPaciente(nome);
-                                
+
                             agenda.voltarMenuPrincipal();
                             flagVoltar = true;
 
                             break;
                         case 4:
                             agenda.listarTodasConsultas();
-                                
+
                             agenda.voltarMenuPrincipal();
                             flagVoltar = true;
 
@@ -236,7 +254,7 @@ public class Main  {
                     System.exit(0);
                     break;
 
-            }    
+            }
         }
 
     }
